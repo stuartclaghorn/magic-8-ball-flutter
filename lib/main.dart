630f8c1b8c686d928ba221098a7267e78f2b49ba
Stuart Claghorn <sclaghorn@gmail.com>
2019-06-05 18:11:27 -0400

    Complete version for self - boss challenge - Udemy course flutter/dart

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blue[300],
+          appBar: AppBar(
+            // centerTitle: true,
+            title: Text('Ask Me Anything!'),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: BallPage(),
+        ),
+      ),
+    );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                });
+                // print('Shake ball.');
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
